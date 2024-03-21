13011d98db8ec920023799bbe223afb369958a72
SebastianTenorio <jaime.tenorio@welldex.com>
2024-03-21 00:01:28 -0500

    creation of state to get the products from the service Product.ts, using useEffect

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,5 +1,16 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { UseProduct } from '../types/UseProduct';
+import { getProducts } from '../api/services/product';
+import { Product } from '../interfaces/Product';
 
-export const useProduct = () =>{
-
+export const useProduct = (): UseProduct => {
+    const [products, setProducts] = useState<Product[]>([]);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const productsRes = await getProducts();
+            setProducts(productsRes);
+        };
+        fetchProducts();
+    }, []);
+    return { products };
 }
