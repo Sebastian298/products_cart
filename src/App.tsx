27ddcb94c46e5ffa62976811e2beae5be46ccb2c
SebastianTenorio <jaime.tenorio@welldex.com>
2024-03-21 00:05:39 -0500

    Refactor product rendering in App component testing a products state from useProduct Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-//import { getProducts } from "./api/services/product";
 import { Header } from "./components/Header";
 import { Product } from "./components/Product";
+import {useProduct} from './hooks/useProduct'
 export const App = () => {
+  const {products} = useProduct();
   return (
     <>
       <Header />
       <main className="container-xl mt-5">
         <h2 className="text-center">Our Products</h2>
         <div className="row mt-5">
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          {products.map((product) => (
+            <Product key={product.id} />
+          ))}
+          
         </div>
       </main>
       <footer className="bg-dark mt-5 py-5">
