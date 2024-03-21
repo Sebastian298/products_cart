593a25c6f354cbe6d491530e9913d97afc3606a1
SebastianTenorio <jaime.tenorio@welldex.com>
2024-03-20 23:48:36 -0500

    Update Product import in product.ts

--- src/api/services/product.ts
+++ src/api/services/product.ts
@@ -1,4 +1,4 @@
-import { Product } from "../../types/Product";
+import { Product } from '../../interfaces/Product';
 export const getProducts = async (): Promise<Product[]> => {
     const response = await fetch('https://fakestoreapi.com/products');
     const data = await response.json();
